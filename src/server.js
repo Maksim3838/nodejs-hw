e6f31796a8687bea3313e10862ea8342811cc311
Maksim <70702008m@gmail.com>
2026-09-13 15:20:24 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: 'Retrieved note with ID: id_param',
+  });
+});
+
+// Тестовий маршрут для перевірки обробки помилок
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware для неіснуючих маршрутів
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

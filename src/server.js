9402657419ca417d00ffaa8ca5bf85036f8259e3
Maksim <70702008m@gmail.com>
2026-09-18 00:40:47 +0300

    err

--- src/server.js
+++ src/server.js
@@ -2,12 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import pinoHttp from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
+const PORT = process.env.PORT ?? 3000;
 
-app.use(cors());
-app.use(express.json());
 app.use(pinoHttp());
+app.use(logger);         // 1. Логер першим — бачить усі запити
+app.use(express.json()); // 2. Парсинг JSON-тіла
+app.use(cors());         // 3. Дозвіл для запитів з інших доменів
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
@@ -23,26 +30,13 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
-// Тестовий маршрут для перевірки обробки помилок
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
 
-// Middleware для неіснуючих маршрутів
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
 
-// Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
-  });
-});
-
-const PORT = process.env.PORT ?? 3000;
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

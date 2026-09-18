59b81d3168a47e032e40e9320d649c581ee96a58
Maksim <70702008m@gmail.com>
2026-09-18 05:10:03 +0300

    fix server middleware order

--- src/server.js
+++ src/server.js
@@ -1,47 +1,29 @@
-import express from 'express';
-import cors from 'cors';
-import pinoHttp from 'pino-http';
-import 'dotenv/config';
-import { connectMongoDB } from './db/connectMongoDB.js';
 
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+import { connectMongoDB } from './db/connectMongoDB.js'
+
+import { logger } from './middleware/logger.js'
+import { notFoundHandler } from './middleware/notFoundHandler.js'
+import { errorHandler } from './middleware/errorHandler.js'
 import notesRouter from './routes/notesRoutes.js'
 
+const app = express()
+const PORT = process.env.PORT ?? 3000
 
+app.use(logger)
+app.use(express.json())
+app.use(cors())
 
-const app = express();
-const PORT = process.env.PORT ?? 3000;
+app.use('/notes', notesRouter)
 
-app.use(notesRouter)
-app.use(pinoHttp());
-app.use(logger);         // 1. Логер першим — бачить усі запити
-app.use(express.json()); // 2. Парсинг JSON-тіла
-app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+app.use(notFoundHandler)
+app.use(errorHandler)
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-// 404 — якщо маршрут не знайдено
-app.use(notFoundHandler);
-
-// Error — якщо під час запиту виникла помилка
-app.use(errorHandler);
-
-await connectMongoDB();
+await connectMongoDB()
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})

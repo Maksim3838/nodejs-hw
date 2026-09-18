f8683cff5182434f7d2eecf532e29bed40343a64
Maksim <70702008m@gmail.com>
2026-09-18 07:26:37 +0300

    fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,14 @@
+
+import { HttpError } from 'http-errors'
+
 export const errorHandler = (err, req, res, next) => {
- console.error(err);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    })
+  }
 
- const isProd = process.env.NODE_ENV === "production";
-
- res.status(500).json({
- message: isProd
-? "Something went wrong. Please try again later."
- : err.message,
- });
+  res.status(500).json({
+    message: err.message,
+  })
 }

b5e1f76bd30f5c773a8d86903a26ca728c1498fe
Maksim <70702008m@gmail.com>
2026-09-22 21:39:47 +0300

    fix middleware and add http-errors

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { errors } from "celebrate";
+import { errors } from 'celebrate'
 
 import { connectMongoDB } from './db/connectMongoDB.js'
 
@@ -17,9 +17,12 @@ const PORT = process.env.PORT ?? 3000
 app.use(logger)
 app.use(express.json())
 app.use(cors())
+
 app.use(notesRouter)
-app.use(notFoundHandler)
+
 app.use(errors())
+
+app.use(notFoundHandler)
 app.use(errorHandler)
 
 await connectMongoDB()

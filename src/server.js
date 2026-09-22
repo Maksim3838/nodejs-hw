ee25420e78b8142cf348108d59c1abf57b523351
Maksim <70702008m@gmail.com>
2026-09-22 21:34:23 +0300

    fix celebrate errors middleware

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ import { logger } from './middleware/logger.js'
 import { notFoundHandler } from './middleware/notFoundHandler.js'
 import { errorHandler } from './middleware/errorHandler.js'
 import notesRouter from './routes/notesRoutes.js'
-import { errors } from 'celebrate'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000

eb7262131a3ea566e1452ddf45bcada8ad7896ae
Maksim <70702008m@gmail.com>
2026-09-26 15:15:36 +0300

    text

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT ?? 3000
 app.use(logger)
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({origin:"*"}))
 
 app.use(notesRouter)
 app.use('/auth', authRoutes)
